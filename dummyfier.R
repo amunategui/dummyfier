6092716eb1950d2c0748eb1ee83a6962c7426ef9
Manuel Amunategui <amunategui@gmail.com>
2014-04-24 06:31:33 -0700

    better comments

--- dummyfier.R
+++ dummyfier.R
@@ -1,6 +1,7 @@
 dummyfier <- function(thedataframe,theColumnName) {
         # Function wrapper for Caret's dummyVars to quickly break out every factor for a given data.frame column
         # Handles one column formula or 2 or more levels and returns altered data.frame with new columns minus original
+        # Automatically builds formula, appends new columns to data.frame and removes old one
         # example use:
         # dummyfier(df,  'FLAG') -- 2 vars
         # dummyfier(df,  'CHIEFCOMPLAINT') -- multi vars
