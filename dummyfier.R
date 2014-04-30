4c8451b065e0541aa69d66688aaa9225fbd5a732
Manuel Amunategui <amunategui@gmail.com>
2014-04-30 11:53:14 -0700

    ability for two var formula

--- dummyfier.R
+++ dummyfier.R
@@ -1,31 +1,34 @@
-dummyfier <- function(thedataframe,theColumnName) {
+dummyfier <- function(theDataFrame, theColumnName, alternateIndex=NULL) {
         # Function wrapper for Caret's dummyVars to quickly break out every factor for a given data.frame column
-        # Handles one column formula on 2 or more levels and returns altered data.frame with new columns minus original
+        # Handles one column formula of 2 or more levels and returns altered data.frame with new columns minus original
+        # Defaults to row index but can be assigned an alternative index in situations where row isn't unique
         # Automatically builds formula, appends new columns to data.frame and removes old one
         # example use:
-        # dummyfier(df,  'FLAG') -- 2 vars
-        # dummyfier(df,  'CHIEFCOMPLAINT') -- multi vars
+        # dummyfier(df, 'FLAG') -- 2 vars
+        # dummyfier(df, 'CHIEFCOMPLAINT') -- multi vars
         
-        # requires the caret library
-        require(caret)
-        
-        if (length(levels(thedataframe[,theColumnName])) == 2) {
-                print(paste("Levels found:",length(levels(thedataframe[,theColumnName]))))
-                thedataframe[,theColumnName] <- ifelse(thedataframe[,theColumnName] == levels(thedataframe[,theColumnName])[1], 1, 0)
+        if (length(levels(theDataFrame[,theColumnName])) == 2) {
+                print(paste("Levels found:",length(levels(theDataFrame[,theColumnName]))))
+                theDataFrame[,theColumnName] <- ifelse(theDataFrame[,theColumnName] == levels(theDataFrame[,theColumnName])[1], 1, 0)
         }
-        else if (length(levels(thedataframe[,theColumnName])) > 2) {
-                print(paste("Levels found:",length(levels(thedataframe[,theColumnName]))))
-                thedataframe[,theColumnName] <- droplevels(thedataframe[,theColumnName])
-                dummies <- dummyVars(paste("~", theColumnName), data = thedataframe)
-                tempdata <- data.frame(predict(dummies, newdata = thedataframe))
+        else if (length(levels(theDataFrame[,theColumnName])) > 2) {
+                print(paste("Levels found:",length(levels(theDataFrame[,theColumnName]))))
+                theDataFrame[,theColumnName] <- droplevels(theDataFrame[,theColumnName])
+                if (!is.null(alternateIndex)) {
+                        dummies <- dummyVars(paste("~", theColumnName, "+", alternateIndex), data = theDataFrame)
+                }
+                else {
+                        dummies <- dummyVars(paste("~", theColumnName), data = theDataFrame)
+                }
+                tempdata <- data.frame(predict(dummies, newdata = theDataFrame))
                 # join new data to data.frame
-                thedataframe <- cbind(thedataframe, tempdata)
+                theDataFrame <- cbind(theDataFrame, tempdata)
                 # drop original column
-                var.out<- setdiff(names(thedataframe),c(theColumnName))
-                thedataframe <- thedataframe[var.out]
+                var.out<- setdiff(names(theDataFrame),c(theColumnName))
+                theDataFrame <- theDataFrame[var.out]
         }
         else {
                 print("No suitable levels")
         }
-        return (thedataframe)
+        return (theDataFrame)
 } 
